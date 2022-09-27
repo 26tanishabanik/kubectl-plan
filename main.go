b81b70a65c7b512c5a139c46bce3a1af6f09e9b2
Tanisha Banik <26tanishabanik@gmail.com>
2022-09-27 13:52:49 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	// "gopkg.in/yaml.v2"
+	"io/ioutil"
+	"fmt"
+	"os"
+	"bufio"
+	"strings"
+	"path/filepath"
+	
+)
+
+func main() {
+	var folder string
+	if len(os.Args) > 0{
+		folder = os.Args[1]
+	}else{
+		fmt.Println("First argument is the folder path")
+	}
+	files, err := ioutil.ReadDir(folder)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    for _, file := range files {
+		extension := filepath.Ext(file.Name())
+		if extension == ".yaml" {
+			file, err := os.Open(file.Name())
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer file.Close()
+
+			scanner := bufio.NewScanner(file)
+			var kind string
+			var count int = 0
+			var statement string = ""
+			for scanner.Scan() {
+				splitted := strings.Split(scanner.Text(), ":")
+				if splitted[0] == "kind" {
+					kind = strings.TrimSpace(splitted[1])
+				}
+				if kind == "Service" {
+					if strings.TrimSpace(splitted[0]) == "type"{
+						statement = statement + " of type " + strings.TrimSpace(splitted[1])
+					}
+					if strings.TrimSpace(splitted[0]) == "name" {
+						statement = "A service with name " + statement + strings.TrimSpace(splitted[1]) + " will be created"
+					}
+				}else if kind == "Deployment" {
+					if strings.TrimSpace(splitted[0]) == "replicas"{
+						statement = statement + " with number of replicas " + strings.TrimSpace(splitted[1])
+					}
+					if count < 1 {
+						if strings.TrimSpace(splitted[0]) == "name" {
+							count += 1
+							statement = "A deployment with name " + statement + strings.TrimSpace(splitted[1]) + " will be created"
+						}
+					}
+				}	
+			}
+			fmt.Println(statement)
+
+			if err := scanner.Err(); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+    
+}
